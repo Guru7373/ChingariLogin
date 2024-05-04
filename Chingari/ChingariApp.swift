e82b674bf673362e71f557451e48f45b956659f5
rgururajr <gururajr7373@gmail.com>
2024-05-04 14:10:15 +0530

    Initial Commit

---
+++ Chingari/ChingariApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChingariApp.swift
+//  Chingari
+//
+//  Created by Guru on 04/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChingariApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
